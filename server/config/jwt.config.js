b3a1a0e1826c0e8f0d1b86a75eaa77c4774b95e3
flogertciku <floggertciku@gmail.com>
2023-10-03 19:20:17 +0200

    401Error

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -1,12 +1,9 @@
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
-const secret = "I can't believe this key is so secret!";
-module.exports.secret = secret;
 module.exports.authenticate = (req, res, next) => {
-    // console.log(req)
+    console.log(req)
   jwt.verify(req.cookies.usertoken, process.env.FIRST_SECRET_KEY, (err, payload) => {
     if (err) { 
-        console.log(err)
       return res.status(401).json({verified: false});
     } else {
       next();
